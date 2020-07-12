1393e518dd0646c9afec48e920d80cfd848f4996
윤도희 <dreamingcats2@gmail.com>
2020-07-12 16:17:58 +0900

    200712 | add new router and views of signup, login, main page | DH

--- myfood/src/router/index.js
+++ myfood/src/router/index.js
@@ -1,24 +1,36 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Main",
+    component: () =>
+      import ("../views/Main.vue")
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/signup",
+    name: "Signup",
+    component: () =>
+      import("../views/Signup.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/Login.vue")
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: () =>
+      import("../views/Logout.vue")
+  },
 ];
 
 const router = new VueRouter({
